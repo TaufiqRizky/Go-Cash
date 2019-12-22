dfbfa82446cc40a59af73e5c1abf73b1c3455d9d
TaufiqRizky <taufiq.it2@gmail.com>
2019-12-22 09:57:27 +0700

    tambah Sidebar menu + fragment

--- app/src/main/java/com/unikom/go_cash/MainActivity.java
+++ app/src/main/java/com/unikom/go_cash/MainActivity.java
@@ -1,14 +1,70 @@
 package com.unikom.go_cash;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PemasukanFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_pemasukan);
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_pemasukan :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PemasukanFragment()).commit();
+                break;
+            case R.id.nav_pengeluaran :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PengeluaranFragment()).commit();
+                break;
+            case R.id.nav_report :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReportFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/unikom/go_cash/PengeluaranFragment.java
@@ -0,0 +1,19 @@
+package com.unikom.go_cash;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PengeluaranFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pengeluaran,container,false);
+    }
+}
