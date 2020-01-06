357e3411879957988a45843dbccfcbbd8b681bc7
Aenik Nur Aufa <aeniknuraufa@users.noreply.github.com>
2020-01-06 08:42:07 +0700

    Tampilan sign in dan sign up

---
+++ app/src/main/java/com/unikom/go_cash/SigninActivity.java
@@ -0,0 +1,49 @@
+package com.unikom.go_cash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SigninActivity extends AppCompatActivity {
+    Dialog regisDialog;
+    private Button btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        regisDialog = new Dialog(this);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowPopup(v);
+            }
+        });
+    }
+    public  void  ShowPopup(View v){
+        TextView txtClose;
+        EditText edtName,edtUsername,edtPassword;
+        Button btnRegister;
+        regisDialog.setContentView(R.layout.activity_register);
+        txtClose = (TextView) regisDialog.findViewById(R.id.txtClose);
+        edtName = (EditText) regisDialog.findViewById(R.id.edtName);
+        edtUsername = (EditText) regisDialog.findViewById(R.id.edtUsername);
+        edtPassword = (EditText) regisDialog.findViewById(R.id.edtPassword);
+        btnRegister = (Button) regisDialog.findViewById(R.id.btnRegister);
+        txtClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regisDialog.dismiss();
+            }
+        });
+
+        regisDialog.show();
+    }
+}
+
