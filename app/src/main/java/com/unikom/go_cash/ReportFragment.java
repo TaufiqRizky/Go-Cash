cfad8bcd14f93230bf33ec09d1495e30bef2ce18
TaufiqRizky <taufiq.it2@gmail.com>
2020-01-13 22:57:21 +0700

    list view Laporan

--- app/src/main/java/com/unikom/go_cash/ReportFragment.java
+++ app/src/main/java/com/unikom/go_cash/ReportFragment.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -25,6 +27,8 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.unikom.go_cash.Adapter.CardAdapter;
+import com.unikom.go_cash.Model.Planet;
 
 import java.util.ArrayList;
 
@@ -34,6 +38,9 @@ public class ReportFragment extends Fragment {
     BarChart barChart;
     Dialog grafikDialog;
     private Button btnGrafik;
+    private RecyclerView recyclerView;
+    private CardAdapter adapter;
+    private ArrayList<Planet> planetArrayList;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
@@ -57,6 +64,8 @@ public class ReportFragment extends Fragment {
                 ShowGrafik(v);
             }
         });
+
+        initView(view);
 
 
 
@@ -83,27 +92,48 @@ public class ReportFragment extends Fragment {
 
     }
 
-//    public class MyAxisValueFormater extends ValueFormatter implements IAxisValueFormatter{
-//        private  String[] mValues;
-//        public  MyAxisValueFormater(String [] values){
-//            this.mValues=values;
-//        }
-//        @Override
-//        public String getFormattedValue(float value, AxisBase axis) {
-//            return mValues[(int)value];
-//        }
-//    }
+    private void initView(View v) {
+        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        planetArrayList = new ArrayList<>();
+        adapter = new CardAdapter(this, planetArrayList);
+        recyclerView.setAdapter(adapter);
+        // recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        createListData();
+    }
+
+    private void createListData() {
+        Planet planet = new Planet("Earth", 150, 10, 12750);
+        planetArrayList.add(planet);
+        planet = new Planet("Jupiter", 778, 26, 143000);
+        planetArrayList.add(planet);
+        planet = new Planet("Mars", 228, 4, 6800);
+        planetArrayList.add(planet);
+        planet = new Planet("Pluto", 5900, 1, 2320);
+        planetArrayList.add(planet);
+        planet = new Planet("Venus", 108, 9, 12750);
+        planetArrayList.add(planet);
+        planet = new Planet("Saturn", 1429, 11, 120000);
+        planetArrayList.add(planet);
+        planet = new Planet("Mercury", 58, 4, 4900);
+        planetArrayList.add(planet);
+        planet = new Planet("Neptune", 4500, 12, 50500);
+        planetArrayList.add(planet);
+        planet = new Planet("Uranus", 2870, 9, 52400);
+        planetArrayList.add(planet);
+        adapter.notifyDataSetChanged();
+    }
 
     public void ShowGrafik(View v){
         TextView txtClose;
         grafikDialog.setContentView(activity__graph);
-        txtClose = (TextView) grafikDialog.findViewById(R.id.txtClose);
-        txtClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                grafikDialog.dismiss();
-            }
-        });
+//        txtClose = (TextView) grafikDialog.findViewById(R.id.txtClose);
+//        txtClose.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                grafikDialog.dismiss();
+//            }
+//        });
         BarChart barChart= grafikDialog.findViewById(R.id.barchart);
         Activity_Graph a = new Activity_Graph();
         a.buatGrafik(barChart);
