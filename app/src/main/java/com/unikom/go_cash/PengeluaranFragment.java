b565f8cec8aa768cee064e78879082ad0c60ed3f
nikoyama <nikoyama32@gmail.com>
2020-01-16 23:12:34 +0700

    add tampilan tambah catatan pengeluaran

--- app/src/main/java/com/unikom/go_cash/PengeluaranFragment.java
+++ app/src/main/java/com/unikom/go_cash/PengeluaranFragment.java
@@ -1,9 +1,12 @@
 package com.unikom.go_cash;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +22,8 @@ import java.util.ArrayList;
 public class PengeluaranFragment extends Fragment {
     private RecyclerView rvPengeluaran;
     private ArrayList<Pengeluaran> list = new ArrayList<>();
+    private Button btnAdd;
+    Dialog addDialog;
 
     @Nullable
     @Override
@@ -27,6 +32,28 @@ public class PengeluaranFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_pengeluaran,container, false);
         rvPengeluaran = view.findViewById(R.id.rv_pengeluaran);
         rvPengeluaran.setHasFixedSize(true);
+        addDialog = new Dialog(getActivity());
+        btnAdd= (Button) view.findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowPopup(v);
+            }
+        });
+//        btnAdd = view.findViewById(R.id.btnAdd);
+//        btnAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showPopUp();
+//            }
+//        });
+        btnAdd = (Button) view.findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowPopup(v);
+            }
+        });
 
         list.addAll(PengeluaranData.getListData());
         showRecyclerList();
@@ -45,6 +72,19 @@ public class PengeluaranFragment extends Fragment {
 //    }
 //
 //
+    public  void  ShowPopup(View v){
+        TextView txtExit;
+        addDialog.setContentView(R.layout.fragment_tambah_pengeluaran);
+        txtExit = (TextView) addDialog.findViewById(R.id.txtExit);
+        txtExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDialog.dismiss();
+            }
+        });
+        addDialog.show();
+    }
+
     private void showRecyclerList() {
         rvPengeluaran.setLayoutManager(new LinearLayoutManager(getActivity()));
         PengeluaranAdapter lpd = new PengeluaranAdapter(list);
